19109bfb1e000f668012f727000b168c915a4f15
frankboi788-source <frankboi788@gmail.com>
2025-08-19 06:38:25 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: '𝙁𝙏 Boi,*Frank  𝙁𝙏 ༆  ༆ 𓃠',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "𝙁𝙏 Boi,*Frank  𝙁𝙏 ༆  ༆ 𓃠",
+  botOwner: '𝙁𝙏 Boi,*Frank  𝙁𝙏 ༆  ༆ 𓃠', // Your name
+  ownerNumber: '27783583068', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This is a bot for managing group commands and automating tasks 𝙁𝙏 Boi,*Frank  𝙁𝙏 ༆  ༆ 𓃠.",
   version: "2.0.8",
 };
 
